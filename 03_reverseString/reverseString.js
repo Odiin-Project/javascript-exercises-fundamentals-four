70006d42473449abf332757bc85cb88f7fec4e08
Annabelle <annabellellis1999@gmail.com>
2023-01-09 10:27:30 -0500

    reversed a string, implemented two ways

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    // Method 1
+    // let reversedString = '';
+    // for(let letter of str){
+    //     reversedString = letter + reversedString;
+    // }
 
+    // return reversedString;
+    
+    // Method 2
+    return str.split("").reverse().join("");
 };
 
 // Do not edit below this line
